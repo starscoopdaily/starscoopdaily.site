ae4166ddaa34e280ab8f015d5c7a66917fb80e6b
somen2k0 <careergrowthremotely@gmail.com>
2026-06-30 12:11:55 +0530

    Allow images from any domain

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.pexels.com' },
-      { protocol: 'https', hostname: '**.pexels.com' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'raw.githubusercontent.com' },
-      { protocol: 'https', hostname: '**.githubusercontent.com' },
-      { protocol: 'https', hostname: '**.cloudinary.com' },
-      { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: '**' },
     ],
   },
 };
